88a93e898698ffdaacb3239161a03b50589b829e
Sanya MARYNYCH <sanyamarynych@o-s-i.org>
2022-11-13 01:20:18 +0200

    - added base components

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Route, Link, Routes } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<Homepage />} />
+                            <Route exact path="/exchanges" element={<Exchanges />} />
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route exact path="/crypto:coinId" element={<CryptoDetails />} />
+                            <Route exact path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
